7c011a37e7f477fc1086b1ea2105569921cfa599
NguyenSon0403 <son2004ntt@gmail.com>
2025-10-13 22:52:47 +0700

    add module Annoumcement and Profile

--- Server/middleware/authMiddleware.js
+++ Server/middleware/authMiddleware.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-
 exports.verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) return res.status(401).json({ message: "Chưa đăng nhập" });
@@ -7,7 +6,7 @@ exports.verifyToken = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // ✅ chứa id, email, role
+    req.user = decoded;
     next();
   } catch (error) {
     res.status(403).json({ message: "Token không hợp lệ" });
